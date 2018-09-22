ae9f92ebc4112acbd397e3ec0ade9f07821e1883
Your Name <you@example.com>
2018-09-22 11:20:52 -0500

    initial

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>
+            Prueba Camilo López
+        </title>
+        <body>
+            <?php echo '<p> Hola Mundo!! Camilo<p>' ;?>
+        </body>
+    </head>
+</html>
